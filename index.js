8021492f17836c3b910cc1f3f2b0c842e9ea3d52
Rick Delhommer <rdelhommer@gmail.com>
2017-08-18 13:38:30 -0700

    add package entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+var SubNetworkEmailer = require('./lib/sub-network-emailer');
+var HelpWantedEmailer = require('./lib/help-wanted-emailer');
+
+module.exports = {
+  SubNetworkEmailer: SubNetworkEmailer,
+  HelpWantedEmailer: HelpWantedEmailer
+};
